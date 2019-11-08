32b301fb30edd4fe38db7fd0eb870050b1cfff5e
scottbernard <scott.bernard@hotmail.ca>
2019-11-08 15:31:35 -0800

    Update driving_NN.py

--- driving_NN.py
+++ driving_NN.py
@@ -1,13 +1,34 @@
+#References:
+# 1. http://neuralnetworksanddeeplearning.com/chap1.html?fbclid=IwAR0NUN7qaHHndp1bhm3fKGkz9DdHvZ-Dobg_UFPY3nU7i1IgW11WiB3TErs
+# 2. https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
+
 import numpy as np
+import time
 
-class NeuralNetwork(object):
-    def __init__(self):
-            return
-    def sigmoid(self, x):
-        return np.exp(x) / (1 + np.exp(x))
+def sigmoid(x):
+	return np.exp(x) / (1 + np.exp(x))
 
-    def sigmoid_derivative(self, x):
-        return self.sigmoid(x)*(1- self.sigmoid(x))
+def sigmoidPrime(self, x):
+	return self.sigmoid(x) * (1 - self.sigmoid(x))
+		
+class NeuralNetwork:
+	# This class creates a basic fully-connected neural network.
+	# The input to the class constructor is the topology of the network: a list of layers containing the number of neurons in that layer.
+	# Example: NeuralNetwork([3, 4, 2]) creates a network with 3 neurons in the first layer, 4 in the second and 2 in the final layer.
+	
+	def __init__(self, topology):
+		self.numLayers = len(topology)
+		self.layers = topology
+		self.neurons = [np.zeros(x) for x in self.layers[0:]] #create list of lists corresponding to the value of each neuron
+		self.weights = [np.random.randn(y, x) for x, y in zip(self.layers[:-1], self.layers[1:])] # create list of 2d lists corresponding to all weights
+		self.biases = [np.random.randn(x, 1) for x in self.layers[1:]] #create list of lists corresponding to the bias of each neuron
+
+	def feedForward(self, input):
+		self.neurons[0] = input
+		for i in range(1, len(self.layers)):
+			for j in range(0, len(self.neurons[i])):
+				self.neurons[i][j] = sigmoid(np.dot(self.neurons[i - 1], self.weights[i - 1][j]) + self.biases[i - 1][j])
+		return self.neurons[-1]
 
     def compute_activations(self, x, W, B):
         a = x
@@ -26,7 +47,7 @@ class NeuralNetwork(object):
         delta_w = [np.zeros(w.shape) for w in W]
 
         ##compute error in output neuron
-        delta_out = self.sigmoid_derivative(z_s[-1]) * (y - a_s[-1])
+        delta_out = self.sigmoidPrime(z_s[-1]) * (y - a_s[-1])
 
         ##initialize last element of delta_b and delta_w
         delta_b[-1] = delta_out
