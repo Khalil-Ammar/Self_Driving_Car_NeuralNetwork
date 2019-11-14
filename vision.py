5bff8d49b027e9932219b4cb7199227ae91195c5
scottbernard <scott.bernard@hotmail.ca>
2019-11-14 13:39:24 -0800

    Add files via upload

--- vision.py
+++ vision.py
@@ -11,6 +11,8 @@ CAMERA_SOURCE = 0
 IMG_RESCALE = 4
 
 SHOW_PROCESSING_WINDOWS = False
+SHOW_CURVE_PROCESSING_WINDOWS = False
+CURVED_LANE_DETECTION = False
 SHOW_RESULT_WINDOW = False
 HARDWARE_ENABLE = True
 PRINT_DIRECTION = False
@@ -30,6 +32,9 @@ def recordingSetup():
     return out
 ###/SETUP FUNCTIONS
 
+def distance(a, b):
+    return np.sqrt((a[1] - b[1])**2 + (a[0] - b[0])**2)
+
 ###RUNTIME FUNCTIONS
 #COLOUR_FILTER
 def colourFilter(img):
@@ -37,17 +42,18 @@ def colourFilter(img):
     #   1. Convert to HLS colourspace
     #   2. Filter out specified colour range
     #   4. Return black/white filtered image
-    
+    # [0, 0, 20]. [255, 106, 110].
+    # [60, 40, 60]. [165, 135, 255].   
     hls = cv.cvtColor(img, cv.COLOR_BGR2HLS)
-    lowerHLSThreshold = np.array([0, 0, 0])
-    upperHLSThreshold = np.array([255, 161, 112])
+    lowerHLSThreshold = np.array([60, 40, 60])
+    upperHLSThreshold = np.array([165, 135, 255])
     hlsFiltered = cv.inRange(hls, lowerHLSThreshold, upperHLSThreshold)
 
     return hlsFiltered
 #/COLOUR_FILTER
 
 #PERSPECTIVE_TRANSFORM
-def perspectiveTransform(img):
+def perspectiveTransform(img, heightScale, widthScale):
     #Function overview:
     #   1. Expand image width to reduce losses from original image after transform
     #   2. Compute transform homography
@@ -58,8 +64,8 @@ def perspectiveTransform(img):
     imgHeight = img.shape[0]
     
     #Construct the indexes of a trapezoid as the homography:
-    widthScale = 0.22
-    heightScale = 0.6
+    #0.2, 0.7
+    #0.35, 0.26
     offset = imgWidth / 2
     initialPoints = np.float32([[int(widthScale * imgWidth), int(heightScale * imgHeight)],
                                 [int((1 - widthScale) * imgWidth), int(heightScale * imgHeight)],
@@ -101,7 +107,7 @@ def detectCentroids(img):
 #/DETECT_CENTROIDS
 
 #CLASSIFY_LANE_POINTS
-def classifyLanePoints(lanePoints):    
+def classifyLanePointsSimple(lanePoints):    
     #Function overview:
     #   1. Sort points based on x value
     #   2. Group points based on x value using a threshold
@@ -119,7 +125,7 @@ def classifyLanePoints(lanePoints):
             #First pass: [1, 2], 3, 10, 11, 12. [1, 2] same line, pop 1 to leftLane
             #Second pass: [2, 3], 10, 11, 12. [2, 3] same line, pop 2 to leftLane
             #Third pass: [3, 10], 11, 12. [3, 10] not same line, pop 3 to leftLane, remaining go to rightLane
-            if abs(sortedLanePoints[1][0] - sortedLanePoints[0][0]) < groupingDistanceThreshold:
+            if abs(sortedLanePoints[0][0] - sortedLanePoints[1][0]) < groupingDistanceThreshold:
                 #Points on same line, pop to leftLane, continue
                 leftLane.append(sortedLanePoints.pop(0))
                 if len(sortedLanePoints) == 1:
@@ -135,8 +141,66 @@ def classifyLanePoints(lanePoints):
     return leftLane, rightLane
 #/CLASSIFY_LANE_POINTS
 
+def classifyLanePoints(lanePoints):    
+    #Function overview:
+    #   1. Sort points based on y value
+    #   2. Group points based on x value using a threshold
+    #   3. Return grouped points
+    #If only one lane is present, it will be passed as the left lane
+    laneA = []
+    laneB = []
+    groupingDistanceThreshold = 50
+    sortedLanePoints = sorted(lanePoints, key=lambda x: x[1])
+    currentLane = 0
+
+    if len(sortedLanePoints) > 0:
+        while len(sortedLanePoints) > 2:
+            groupingDistanceThreshold = 70
+            if distance(sortedLanePoints[0], sortedLanePoints[1]) < groupingDistanceThreshold:
+                if currentLane == 0:
+                    laneA.append(sortedLanePoints.pop(0))
+                else:
+                    laneB.append(sortedLanePoints.pop(0))
+
+                if len(sortedLanePoints) == 1:
+                    if currentLane == 0:
+                        laneA.append(sortedLanePoints.pop(0))
+                    else:
+                        laneB.append(sortedLanePoints.pop(0))
+                    break
+            else:
+                if currentLane == 0:
+                    laneA.append(sortedLanePoints.pop(0))
+                else:
+                    laneB.append(sortedLanePoints.pop(0))
+
+                if len(sortedLanePoints) == 1:
+                    if currentLane == 0:
+                        laneB.append(sortedLanePoints.pop(0))
+                    else:
+                        laneA.append(sortedLanePoints.pop(0))
+                    break
+                currentLane = 1 - currentLane
+    rightLane = []
+    leftLane = sortedLanePoints
+    if len(laneA) > 0 and len(laneB) > 0:
+        aMin = laneA[0]
+        bMin = laneB[0]
+        if aMin[0] < bMin[0]:
+            leftLane = laneA
+            rightLane = laneB
+        else:
+            leftLane = laneB
+            rightLane = laneA
+    else:
+        if len(laneA) > 0:
+            leftLane = laneA
+        if len(laneB) > 0:
+            leftLane = laneB
+    return leftLane, rightLane
+#/CLASSIFY_LANE_POINTS
 #FIT_LANES
-def fitLanes(leftLanePoints, rightLanePoints):
+def fitLanes(leftLanePoints, rightLanePoints, degree):
     #Function overview:
     #1. Fit straight line to points in left and right lanes
     #2. Return coefficients for these lines
@@ -145,11 +209,11 @@ def fitLanes(leftLanePoints, rightLanePoints):
     if len(leftLanePoints) > 1:
         x = [p[0] for p in leftLanePoints]
         y = [p[1] for p in leftLanePoints]
-        leftCoefs = poly.polyfit(y, x, 1)
+        leftCoefs = poly.polyfit(y, x, degree)
     if len(rightLanePoints) > 1:
         x = [p[0] for p in rightLanePoints]
         y = [p[1] for p in rightLanePoints]
-        rightCoefs = poly.polyfit(y, x, 1)
+        rightCoefs = poly.polyfit(y, x, degree)
     return leftCoefs, rightCoefs
 #/FIT_LANES
 
@@ -165,15 +229,15 @@ def getAvgCoefs(leftCoefs, rightCoefs):
 #/GET_AVG_COEFS
 
 #SHOW_LANE_POINTS
-def showLanePoints(img, lanePoints):
+def showLanePoints(img, lanePoints, title):
     result = img.copy()
     for point in lanePoints:
         cv.circle(result, (point[0], point[1]), 5, (0, 0, 0), -1)
-    cv.imshow("Lane Points", result)
+    cv.imshow(title, result)
 #/SHOW_LANE_POINTS
 
 #SHOW_CLASSIFIED_LANE_POINTS
-def showClassifiedLanePoints(img, leftLanePoints, rightLanePoints):
+def showClassifiedLanePoints(img, leftLanePoints, rightLanePoints, title):
     result = cv.cvtColor(img.copy(), cv.COLOR_GRAY2BGR)
     if len(leftLanePoints) > 0:
         for point in leftLanePoints:
@@ -181,12 +245,12 @@ def showClassifiedLanePoints(img, leftLanePoints, rightLanePoints):
     if len(rightLanePoints) > 0:
         for point in rightLanePoints:
             cv.circle(result, (point[0], point[1]), 5, (0, 0, 255), -1)
-    cv.imshow("Classified Lane Points", result)
+    cv.imshow(title, result)
     return
 #/SHOW_CLASSIFIED_LANE_POINTS
 
 #SHOW_LANE_LINES
-def showLaneLines(img, leftCoefs, rightCoefs, avgCoefs):
+def showLaneLines(img, leftCoefs, rightCoefs, avgCoefs, title):
     result = cv.cvtColor(img.copy(), cv.COLOR_GRAY2BGR)
     x = [0, result.shape[0]]
     if len(leftCoefs) > 0:
@@ -201,13 +265,34 @@ def showLaneLines(img, leftCoefs, rightCoefs, avgCoefs):
         fit = poly.polyval(x, avgCoefs)
         cv.line(result, (int(fit[0]), x[0]), (int(fit[1]), x[1]), (255, 0, 255), 5)
         cv.putText(result, str(avgCoefs[1])[0:5], (0, 60), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1, cv.LINE_AA)
-    cv.imshow("Lane Lines", result)
+    cv.imshow(title, result)
 #/SHOW_LANE_LINES
+
+def showCurveLaneLines(img, leftCoefs, rightCoefs, avgCoefs, title):
+    result = cv.cvtColor(img.copy(), cv.COLOR_GRAY2BGR)
+    x = np.arange(0, img.shape[1], 20)
+    if len(leftCoefs) > 0:
+        fit = poly.polyval(x, leftCoefs)
+        pts = np.stack((fit, x), axis=-1)
+        cv.polylines(result, np.int32([pts]), 0, (255, 0, 0), 5)
+        cv.putText(result, str(leftCoefs[2])[0:5], (0, 20), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1, cv.LINE_AA)
+    if len(rightCoefs) > 0:
+        fit = poly.polyval(x, rightCoefs)
+        pts = np.stack((fit, x), axis=-1)
+        cv.polylines(result, np.int32([pts]), 0, (0, 0, 255), 5)
+        cv.putText(result, str(rightCoefs[2])[0:5], (0, 40), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv.LINE_AA)
+    if len(avgCoefs) > 0:
+        fit = poly.polyval(x, avgCoefs)
+        pts = np.stack((fit, x), axis=-1)
+        cv.polylines(result, np.int32([pts]), 0, (255, 0, 255), 5)
+        cv.putText(result, str(avgCoefs[2])[0:5], (0, 60), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1, cv.LINE_AA)
+    cv.imshow(title, result)
 
 #COMPUTE_RESULTS
 def computeResults(img, direction, warpedShape, leftCoefs, rightCoefs, avgCoefs, iPT):
     warpedLines = np.zeros((warpedShape[0], warpedShape[1], 3), np.uint8)
-    x = [0, warpedLines.shape[1]]
+    x = [0, warpedLines.shape[0]]
+    distanceFromCentre = None
     if len(leftCoefs) > 0:
         fit = poly.polyval(x, leftCoefs)
         cv.line(warpedLines, (int(fit[0]), x[0]), (int(fit[1]), x[1]), (255, 0, 0), 5)
@@ -217,6 +302,7 @@ def computeResults(img, direction, warpedShape, leftCoefs, rightCoefs, avgCoefs,
     if len(avgCoefs) > 0:
         fit = poly.polyval(x, avgCoefs)
         cv.line(warpedLines, (int(fit[0]), x[0]), (int(fit[1]), x[1]), (255, 0, 255), 5)
+        distanceFromCentre = int(fit[1]) - warpedLines.shape[1]/2
     unwarpedLines = cv.warpPerspective(warpedLines, iPT, (warpedLines.shape[1], warpedLines.shape[0]))
     unwarpedLines = unwarpedLines[0:img.shape[0], 0:img.shape[1]]
     
@@ -228,6 +314,7 @@ def computeResults(img, direction, warpedShape, leftCoefs, rightCoefs, avgCoefs,
         cv.putText(result, str(rightCoefs[1])[0:5], (0, 40), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv.LINE_AA)
     if len(avgCoefs) > 0:
         cv.putText(result, str(avgCoefs[1])[0:5], (0, 60), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1, cv.LINE_AA)
+        cv.putText(result, "D: " + str(distanceFromCentre)[0:5], (0, 80), cv.FONT_HERSHEY_PLAIN, 1, (0, 255, 255), 1, cv.LINE_AA)
         
     if direction == 0:
         cv.putText(result, "Stop", (img.shape[1] - int(img.shape[1]/2), 20), cv.FONT_HERSHEY_PLAIN, 1, (255, 255, 0), 1, cv.LINE_AA)
@@ -240,10 +327,39 @@ def computeResults(img, direction, warpedShape, leftCoefs, rightCoefs, avgCoefs,
     return result
 #/COMPUTE_RESULTS
 
+def computeCurveResults(img, warpedShape, leftCoefs, rightCoefs, avgCoefs, iPT):
+    warpedLines = np.zeros((warpedShape[0], warpedShape[1], 3), np.uint8)
+    x = np.arange(int(0.3*img.shape[1]), img.shape[1], 20)
+    if len(leftCoefs) > 0:
+        fit = poly.polyval(x, leftCoefs)
+        pts = np.stack((fit, x), axis=-1)
+        cv.polylines(warpedLines, np.int32([pts]), 0, (255, 0, 0), 5)
+    if len(rightCoefs) > 0:
+        fit = poly.polyval(x, rightCoefs)
+        pts = np.stack((fit, x), axis=-1)
+        cv.polylines(warpedLines, np.int32([pts]), 0, (0, 0, 255), 5)
+    if len(avgCoefs) > 0:
+        fit = poly.polyval(x, avgCoefs)
+        pts = np.stack((fit, x), axis=-1)
+        cv.polylines(warpedLines, np.int32([pts]), 0, (255, 0, 255), 5)
+    unwarpedLines = cv.warpPerspective(warpedLines, iPT, (warpedLines.shape[1], warpedLines.shape[0]))
+    unwarpedLines = unwarpedLines[0:img.shape[0], 0:img.shape[1]]
+    
+    result = cv.addWeighted(img, 0.8, unwarpedLines, 1, 0)
+    
+    if len(leftCoefs) > 0:
+        cv.putText(result, str(leftCoefs[2])[0:5], (0, 20), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1, cv.LINE_AA)
+    if len(rightCoefs) > 0:
+        cv.putText(result, str(rightCoefs[2])[0:5], (0, 40), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 1, cv.LINE_AA)
+    if len(avgCoefs) > 0:
+        cv.putText(result, str(avgCoefs[2])[0:5], (0, 60), cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 255), 1, cv.LINE_AA)
+        
+    return result
+    
 #COMPUTE_DRIVE_DIRECTION
 def computeDriveDirection(avgSlope):
-    leftThreshold = 0.15
-    rightThreshold = -0.15
+    leftThreshold = 0.13
+    rightThreshold = -0.13
     
     if len(avgSlope) == 0:
         return 0
@@ -314,25 +430,25 @@ def main():
 			cv.imshow("Filtered Frame", filteredFrame)
         
         #4. Perform perspective transform
-        warpedFrame, iPT = perspectiveTransform(filteredFrame) #obtain inverse perspective transform
+        warpedFrame, iPT = perspectiveTransform(filteredFrame, 0.7, 0.125) #obtain inverse perspective transform
         if SHOW_PROCESSING_WINDOWS:
 			cv.imshow("Warped Frame", warpedFrame)
         
         #5. Perform centroid detection on horizontal slices of transformed image
         lanePoints = detectCentroids(warpedFrame)
         if SHOW_PROCESSING_WINDOWS:
-			showLanePoints(warpedFrame, lanePoints)
+			showLanePoints(warpedFrame, lanePoints, "Lane Points")
 
         #6. Group centroid points into lanes
         leftLanePoints, rightLanePoints = classifyLanePoints(lanePoints)
         if SHOW_PROCESSING_WINDOWS:
-			showClassifiedLanePoints(warpedFrame, leftLanePoints, rightLanePoints)
+			showClassifiedLanePoints(warpedFrame, leftLanePoints, rightLanePoints, "Classified Lane Points")
         
         #7. Perform curve-fitting to lane points, normalize slopes
-        leftCoefs, rightCoefs = fitLanes(leftLanePoints, rightLanePoints)
+        leftCoefs, rightCoefs = fitLanes(leftLanePoints, rightLanePoints, 1)
         avgCoefs = getAvgCoefs(leftCoefs, rightCoefs)
         if SHOW_PROCESSING_WINDOWS:
-            showLaneLines(warpedFrame, leftCoefs, rightCoefs, avgCoefs)
+            showLaneLines(warpedFrame, leftCoefs, rightCoefs, avgCoefs, "Lane Lines")
             
         #8. Compute driving direction based on average slope of lane lines
         direction = computeDriveDirection(avgCoefs)
@@ -346,6 +462,25 @@ def main():
         #9. Determine hardware output based on driving direction
         drive(direction)
         
+        #10. Curve lane detection
+        if CURVED_LANE_DETECTION:
+            curveWarpedFrame, curveIPT = perspectiveTransform(filteredFrame, 0.26, 0.35) #obtain inverse perspective transform
+            if SHOW_CURVE_PROCESSING_WINDOWS:
+                cv.imshow("Curve Warped Frame", curveWarpedFrame)
+            curveLanePoints = detectCentroids(curveWarpedFrame)
+            if SHOW_CURVE_PROCESSING_WINDOWS:
+                showLanePoints(curveWarpedFrame, curveLanePoints, "Curve Lane Points")
+            leftCurvePoints, rightCurvePoints = classifyLanePoints(curveLanePoints)
+            if SHOW_CURVE_PROCESSING_WINDOWS:
+                showClassifiedLanePoints(curveWarpedFrame, leftCurvePoints, rightCurvePoints, "Classified Curve Lane Points")
+            leftCurveCoefs, rightCurveCoefs = fitLanes(leftCurvePoints, rightCurvePoints, 3)
+            avgCurveCoefs = getAvgCoefs(leftCurveCoefs, rightCurveCoefs) 
+            if SHOW_CURVE_PROCESSING_WINDOWS:
+                showCurveLaneLines(curveWarpedFrame, leftCurveCoefs, rightCurveCoefs, avgCurveCoefs, "Curve Lane Lines")  
+            curveResult = computeCurveResults(resizedFrame, curveWarpedFrame.shape, leftCurveCoefs, rightCurveCoefs, avgCurveCoefs, curveIPT)
+            if SHOW_RESULT_WINDOW:
+                cv.imshow("Curve Result", curveResult)
+            
         k = cv.waitKey(5) & 0xFF
         if k == 27:
             cap.release()
